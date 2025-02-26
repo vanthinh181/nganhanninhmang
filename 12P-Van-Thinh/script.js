b634dd6c8f5fc2605c2eb7d3321cfda033a93351
vanthinh181 <tl23022007@gmail.com>
2025-02-26 17:26:38 +0700

    Add files via upload

---
+++ 12P-Van-Thinh/script.js
@@ -0,0 +1,11 @@
+function login
+{
+  let a= Tên đăng nhập.value
+  let b= Password.value
+  if (a== "abc" && b== "123"
+  {
+      window.open("home.html")
+  }
+  else
+    document.write("Không hợp lệ")
+}
